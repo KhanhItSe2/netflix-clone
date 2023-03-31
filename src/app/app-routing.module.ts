cb2bb8ecc95ff2a01e8bbc2dd539693ffcd0e2df
KhanhItSe2 <khanhnguyenitse@gmail.com>
2023-03-31 21:51:45 +0700

    update redirects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
@@ -22,11 +27,6 @@ const routes: Routes = [
   {
     path: 'forgot-password',
     component: ForgotPasswordComponent,
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
   },
 ];
 
